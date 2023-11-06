71ea25428587d55e6179344045d7a9aa56bcba4f
Uzma <uzma97910@gmail.com>
2023-11-06 21:41:10 +0100

    tailwind customization

--- src/components/Header.js
+++ src/components/Header.js
@@ -36,9 +36,9 @@ export const Header = () => {
   return (
     <header>
       <nav className="bg-white border-b-2 border-gray-200 sm:px-4 py-2.5 dark:bg-gray-900 dark:border-b-1 dark:border-gray-900 ">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
           <Link to="/" className="flex items-center">
-            <img src={Logo} className="h-8 mr-3" alt="Logo" />
+            <img src={Logo} className="h-8 mr-2" alt="Logo" />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
               CineMate
             </span>
